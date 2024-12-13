8148206f13e49a633f099f2d14e8d22dbd63f8b3
zhemepatis <rink.gab@gmail.com>
2024-12-13 18:58:13 +0200

    added fetch

--- lab-04/scripts/sudoku.js
+++ lab-04/scripts/sudoku.js
@@ -1,10 +1,19 @@
-function generateCanvas() {
+function fetchPuzzle(id = 1) {
+    fetch(`https://6550e0cc7d203ab6626e476a.mockapi.io/api/v1/SudokuBoard/${id}`, {
+        method: "GET"
+    })
+    .then(res => res.json())
+    .then(puzzle => generateCanvas(puzzle.board))
+}
+
+function generateCanvas(puzzle) {
     let canvas = $(".sudoku-canvas")
 
     for(let i = 0; i < 81; ++i) {
         let cell = document.createElement("input")
         cell.id = `sudoku-cell-${i}`
         cell.className = "sudoku-cell"
+        cell.setAttribute("autocomplete", "off")
 
         cellCol = i % 9 
         if ((cellCol + 1) % 3 == 0 && cellCol != 8)
@@ -13,6 +22,11 @@ function generateCanvas() {
         cellRow = Math.floor(i / 9)
         if ((cellRow + 1) % 3 == 0 && cellRow != 8)
             cell.className += " bottom-row"
+
+        if (puzzle[i] != "x") {
+            cell.value = puzzle[i]
+            cell.disabled = true
+        }
 
         canvas.append(cell)
     }
@@ -26,12 +40,12 @@ function resetCanvas() {
 }
 
 function resetCell(cell) {
-    cell.value = ""
+    if (!cell.disabled)
+        cell.value = ""
 }
 
-
 $(document).ready(() => {
-    generateCanvas()
+    fetchPuzzle()
 
     $("#reset-canvas-btn").click(() => {
         resetCanvas()
